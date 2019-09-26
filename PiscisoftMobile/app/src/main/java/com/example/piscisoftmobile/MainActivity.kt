838da562835c2786b72c761cc0517cbf8971ead6
Aron <20160795@aloe.ulima.edu.pe>
2019-09-26 14:14:45 -0500

    awodamwod

--- PiscisoftMobile/app/src/main/java/com/example/piscisoftmobile/MainActivity.kt
+++ PiscisoftMobile/app/src/main/java/com/example/piscisoftmobile/MainActivity.kt
@@ -15,7 +15,9 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //aron
 
+        //ayawdaw
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn_registrarse.setOnClickListener{irRegistro()}

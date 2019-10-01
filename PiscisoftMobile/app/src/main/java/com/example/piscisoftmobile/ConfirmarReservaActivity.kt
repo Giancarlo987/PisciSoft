a5a97898ea4a4340e2b43f71fa16eebfa44fcad7
Andrea <20160315@aloe.ulima.edu.pe>
2019-10-01 12:24:28 -0500

    Realizar Reserva (sin validaciones)

---
+++ PiscisoftMobile/app/src/main/java/com/example/piscisoftmobile/ConfirmarReservaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.piscisoftmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmarReservaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmar_reserva)
+    }
+}
---
+++ PiscisoftMobile/app/src/main/java/com/example/piscisoftmobile/Model/Reserva.kt
@@ -0,0 +1,4 @@
+package com.example.piscisoftmobile.Model
+
+class Reserva {
+}

db7b5636a9f96dd6c0e43746e8c3244226fe02cc
Giancarlo <20161333@aloe.ulima.edu.pe>
2019-10-23 16:37:23 -0500

    Web Sprint 3-Falta cambio de color

--- PiscisoftWeb/public/Javascript/Objetos/cookie.js
+++ PiscisoftWeb/public/Javascript/Objetos/cookie.js
@@ -1,6 +1,6 @@
 function MakeCookie() { 
     var usuarioNombre = document.getElementById('nombre').value;
-    var usuarioPassword = document.getElementById('password').value;
+    var usuarioPassword = document.getElementById('password').value
     var store = firebase.firestore();
     var docRef = store.collection("AdministradorUsuario").doc("AtyyZuBXX3ButSAB9OLH");
     docRef.get().then(
@@ -9,6 +9,9 @@ function MakeCookie() {
                 document.cookie = "Login=true; expires= Fri, 01 Dec 9999 00:00:00 GMT";
                 window.location.href = "https://piscisoft.firebaseapp.com/paginaPrincipal.html";
             } else {
+                document.getElementById('nombre').value =""
+                document.getElementById('password').value = ""
+                console.log("g")
                 $(document).ready(function () {
                     $("#ventanaError").modal("show");
                 });
